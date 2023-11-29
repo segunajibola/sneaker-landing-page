e748d3b59308dcec7f142d0ddb03affae1dbaf42
Segun Ajibola <ajbl.segun@gmail.com>
2023-11-29 12:07:03 +0100

    about

--- script.js
+++ script.js
@@ -37,4 +37,39 @@ document.addEventListener('DOMContentLoaded', function () {
     window.addEventListener('scroll', function () {
         checkFade();
     });
+
+    const textArray = ["Hello, World!", "Welcome to my website.", "Enjoy your visit!"];
+    let index = 0; // Index to track the current string in the array
+    let charIndex = 0; // Index to track the current character in the string
+
+    function type() {
+        const typingText = document.getElementById('typewriter');
+        const currentText = textArray[index];
+        typingText.innerHTML = currentText.substring(0, charIndex + 1);
+
+        charIndex++;
+
+        // Check if the current string is fully typed
+        if (charIndex < currentText.length) {
+            setTimeout(type, 100); // Type the next character after a delay
+        } else {
+            // Move to the next string in the array after a longer delay
+            setTimeout(() => {
+                index++;
+                charIndex = 0;
+
+                // Loop back to the first string if all strings are typed
+                if (index === textArray.length) {
+                    index = 0;
+                }
+
+                // Start typing the next string
+                type();
+            }, 2000);
+        }
+    }
+
+    // Start the typing effect when the page loads
+    type()
+
 });
